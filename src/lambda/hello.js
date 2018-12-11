53654611b8e5edfef62473d602692fe18daaae49
Anton Alstes <aalstes@gmail.com>
2018-12-11 17:08:33 +0100

    Now checks context.clientContext.user.

--- src/lambda/hello.js
+++ src/lambda/hello.js
@@ -1,7 +1,13 @@
 
 exports.handler = function(event, context, callback) {
-    callback(null, {
+
+  const claims = context.clientContext && context.clientContext.user;
+  if (!claims) {
+    return callback(null, { statusCode: 401, body: "You must be signed in to call this function" });
+  }
+
+  return callback(null, {
     statusCode: 200,
     body: "Hello, World"
-    });
+  });
 }
